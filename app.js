6398bcbeb6a0ed602286dff2830e0e5ff4c776a0
Christopher Dickes <chris@statesofpop.de>
2017-02-06 21:55:42 +0100

    basic setup for nw.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var RoonApi = require("node-roon-api"),
 var roon = new RoonApi({
     extension_id:        'com.statesofpop.roon-yamaha',
     display_name:        "Yamaha Control",
-    display_version:     "0.0.5",
+    display_version:     "0.0.6",
     publisher:           'states of pop',
     email:               'hi@statesofpop.de',
     website:             'https://github.com/statesofpop/roon-yamaha'
